5f534f48d7a078bd7efc515ac21b5fc81794012e
foreknowledge <kyj_0129@naver.com>
2023-04-08 23:42:33 +0900

    refactor: RandomGenerator 코드 다듬기

--- src/core/logic/RandomGenerator.ts
+++ src/core/logic/RandomGenerator.ts
@@ -5,15 +5,15 @@ import { Type } from '../type/tetromino.types';
  * 다음 Tetromino 나오는 규칙 정의
  */
 export default class RandomGenerator {
-  private bag = ALL_TYPES;
+  private bag: Set<Type> = new Set(ALL_TYPES);
 
   next(): Type {
-    if (this.bag.length === 0) this.bag = ALL_TYPES;
+    if (this.bag.size === 0) this.bag = new Set(ALL_TYPES);
 
-    const draw = randomItem(this.bag);
-    this.bag = this.bag.filter((it) => it !== draw);
+    const nextItem = randomItem([...this.bag]);
+    this.bag.delete(nextItem);
 
-    return draw;
+    return nextItem;
   }
 }
 
